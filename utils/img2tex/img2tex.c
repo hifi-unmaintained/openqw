138719cec661422b341780a2ca2e5d5653c61f97
Toni Spets <toni.spets@iki.fi>
2011-05-19 17:21:18 +0300

    Rename png2tex to img2tex and add support for other GD supported formats: gif, jpg, bmp (and png)

--- utils/img2tex/img2tex.c
+++ utils/img2tex/img2tex.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <gd.h>
 
 #include "palette.h"
@@ -9,6 +10,7 @@ int main(int argc, char **argv)
 	if (argc < 3) {
 		printf("usage: %s <src> <dst> [width] [height]\n", argv[0]);
 		printf("	   %s truecolor256x256.png texture64x64.png 64 64\n", argv[0]);
+		return 1;
 	}
 
 	char *src_path = argv[1];
@@ -16,6 +18,14 @@ int main(int argc, char **argv)
 	int dst_width = -1;
 	int dst_height = -1;
 	int i;
+	gdImagePtr src;
+
+	char *ext = strrchr(src_path, '.');
+        if (ext == NULL) {
+		printf("%s is not a valid image file\n", src_path);
+		return 1;
+	}
+	ext++;
 
 	FILE *src_fh = fopen(src_path, "r");
 	if (!src_fh) {
@@ -29,7 +39,22 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	gdImagePtr src = gdImageCreateFromPng(src_fh);
+	if (strcasecmp(ext, "gif") == 0) {
+		src = gdImageCreateFromGif(src_fh);
+	}
+	else if (strcasecmp(ext, "png") == 0) {
+		src = gdImageCreateFromPng(src_fh);
+	}
+	else if (strcasecmp(ext, "jpg") == 0) {
+		src = gdImageCreateFromJpeg(src_fh);
+	}
+	else if (strcasecmp(ext, "bmp") == 0) {
+		src = gdImageCreateFromWBMP(src_fh);
+	}
+	else {
+		printf("%s is not supported\n", ext);
+		return 1;
+	}
 
 	if (argc > 4) {
 		dst_width = atoi(argv[2]);
